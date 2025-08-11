d4af665c83a8154b1435fb436bf871697905832b
Dillon stevenson <Dillonstevenson3@gmail.com>
2025-08-11 14:38:21 +0100

    mainpage-concept1

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <a href="/">Home</a>
+      <a href="/about">Lessons</a>
+      <a href="/contact" className="account-link">
+        Account
+      </a>
+    </nav>
+  );
+}
