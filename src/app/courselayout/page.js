d29bca5f34e21c23db38b11861f2efb43df70b8a
ebenezet <ebenezer.t@hotmail.fr>
2025-08-13 13:27:06 +0100

    course layout page improved with lessons now individually accessible and added a link to reach the lesson

--- src/app/courselayout/page.js
+++ src/app/courselayout/page.js
@@ -2,6 +2,15 @@ import Link from "next/link";
 import { db } from "@/utils/utilities";
 
 export default async function CourseLayoutPage({}) {
+  // fetching all units from the database
+  const units = (await db.query(`SELECT * FROM units`)).rows;
+  console.log(units);
+
+  // fetching all lessons from the database
+  const lessons = (await db.query(`SELECT * FROM lessons`)).rows;
+  console.log(lessons);
+
+  // fetching all lessons and group them by unit
   const lessonsPerUnits = (
     await db.query(`SELECT unit_name, ARRAY_AGG(lesson_name) AS thelessons
 FROM units
@@ -15,10 +24,12 @@ GROUP BY units.id`)
       {lessonsPerUnits.map((lessonsPerUnit) => (
         <div key={lessonsPerUnit.lesson_id}>
           <h3 className="flex font-bold ">{lessonsPerUnit.unit_name}</h3>
-          <Link href={`/lessons/${lesson.id}`}>
-            {" "}
-            <p>{lessonsPerUnit.thelessons}</p>
-          </Link>
+          {lessonsPerUnit.thelessons.map((lesson) => (
+            <p>
+              {" "}
+              <Link href={`/lessons/${lesson.id}`}> {lesson} </Link>
+            </p>
+          ))}
         </div>
       ))}
     </>
